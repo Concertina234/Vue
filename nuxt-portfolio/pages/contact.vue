83e8c2801619e28b6624693a2d8476b618349596
Concertina234 <hanamaru234@hotmail.co.jp>
2024-11-04 13:51:47 +0900

    [add]Nuxt勉強

---
+++ nuxt-portfolio/pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <h1>Contact</h1>
+      <P>お気軽にご連絡ください</P>
+      <form method="POST">
+        <label htmlFor="name">お名前</label>
+        <input type="text" name="name" id="name" required />
+        <label htmlFor="email">メールアドレス</label>
+        <input type="email" name="email" id="email" required />
+        <label htmlFor="name">ご用件</label>
+        <textarea name="message" rows="10" id="textarea" required></textarea>
+        <button type="submit">送信</button>
+    </form>
+    </div>
+  </div>
+</template>
